da070ec2dcdfa43f49956118bd7813e658e8564d
come25136 <come25136@gmail.com>
2018-03-10 18:28:52 +0900

    :recycle: /libs/route.tsを最適化した
    …わかった！ 君はえっちな人だ！
    
    最適化する人って変態しか居ないよね

--- libs/route.ts
+++ libs/route.ts
@@ -2,9 +2,11 @@ import * as moment from 'moment'
 
 import { isHoliday } from 'japanese-holidays'
 
-import { default as translations, Inames } from '../GTFS_loader/translation'
-import stops from '../GTFS_loader/stops'
-import stopTimes from '../GTFS_loader/stop_times'
+import { default as _translations, Inames } from '../GTFS_loader/translation'
+import { default as _stops, Istop as _Istop } from '../GTFS_loader/stops'
+import { default as _stopTimes, Istop as IstopTimes } from '../GTFS_loader/stop_times'
+
+import { Ierror } from '../interfaces'
 
 interface Istop {
   name: Inames
@@ -13,68 +15,40 @@ interface Istop {
   lon: number
 }
 
-let cache: [
-  Map<string, Inames>,
-  Map<string, {
-    stop_id: string
-    stop_code: string
-    stop_name: string
-    stop_desc: null
-    stop_lat: number
-    stop_lon: number
-    zone_id: string
-    stop_url: null
-    location_type: number
-  }>,
-  Map<string, {
-    [key: string]: {
-      trip_id: string
-      arrival_time: string
-      departure_time: string
-      stop_id: string
-      stop_sequence: number
-      stop_headsign: string
-      pickup_type: number
-      drop_off_type: number
-    }[]
-  }>
-]
+let
+  translations: Map<string, Inames>,
+  stops: Map<string, _Istop>,
+  stopTimes: Map<string, { [key: string]: IstopTimes[] }>
 
 
-export default (line: number | string, date: string) => new Promise<Istop[]>(resolve => {
-  cache
-    ? getSteps()
-    : Promise.all([translations, stops, stopTimes,])
-      .then(data => {
-        cache = data
-        getSteps()
-      })
+export default (line: number | string, _date: string) => new Promise<Istop[]>(async (resolve) => {
+  if (!translations && !stops && !stopTimes)[translations, stops, stopTimes] = await Promise.all([_translations, _stops, _stopTimes])
 
-  function getSteps(): Istop[] | undefined {
-    const
-      [translations, stops, stopTimes] = cache,
-      _date = moment(date),
-      routes = stopTimes.get(`${_date.day() === 0 || _date.day() === 6 || isHoliday(_date.toDate()) ? 'holiday' : 'weekday'}_${line}`)
+  const
+    date = moment(_date),
+    time = date.format('HH:mm'),
+    routes = stopTimes.get(`${date.day() === 0 || date.day() === 6 || isHoliday(date.toDate()) ? 'holiday' : 'weekday'}_${line}`)
 
-    if (!routes) return
+  if (!routes || !routes[time]) {
+    console.log(routes)
 
-    const _time = _date.format('HH:mm')
-
-    if (!routes[_time]) console.log(routes)
-
-    resolve(routes[_time].map(stop_raw => {
-      const stop = stops.get(stop_raw.stop_id) || {
-        stop_name: '',
-        stop_lat: 0,
-        stop_lon: 0
-      }
-
-      return {
-        name: Object.assign({ ja: '', 'ja-Hrkt': '', en: '' }, translations.get(stop.stop_name)),
-        time: moment(stop_raw.arrival_time, 'HH:mm:ss').toISOString(),
-        lat: stop.stop_lat,
-        lon: stop.stop_lon
-      }
-    }))
+    const err: Ierror = new Error('There is no such route')
+    err.code = 404
+    throw err
   }
+
+  resolve(routes[time].map(stop_raw => {
+    const stop = stops.get(stop_raw.stop_id) || {
+      stop_name: '',
+      stop_lat: 0,
+      stop_lon: 0
+    }
+
+    return {
+      name: Object.assign({ ja: '', 'ja-Hrkt': '', en: '' }, translations.get(stop.stop_name)),
+      time: moment(stop_raw.arrival_time, 'HH:mm:ss').toISOString(),
+      lat: stop.stop_lat,
+      lon: stop.stop_lon
+    }
+  }))
 })
