f84ee312b9384da9fec238e3c6b8c69df90731ec
come25136 <come25136@gmail.com>
2018-03-22 00:52:08 +0900

    :sparkles: unobus.ts用のテストコード書いた
    テストしてないAPIを作るのって、やっぱり無責任だからね。

--- test/app.ts
+++ test/app.ts
@@ -1,48 +1,104 @@
-import * as ioClient from 'socket.io-client'
+import { readdir, readFile, appendFileSync } from 'fs'
 
-interface Inames {
-  ja: string
-  'ja-Hrkt': string
-  en: string
-}
+import * as csvParse from 'csv-parse'
 
-interface Ibus {
+import * as moment from 'moment'
+
+import * as unobus from '../libs/unobus'
+
+import { Ibus } from '../interfaces'
+
+interface IbusRaw {
   route_num: number
   okayama_stop_time: string
   delay: number
-  run: boolean
+  run: string
+  passing_stop: string
   license_number: number
-  location: {
-    lat: number
-    lon: number
-  }
-  stops: {
-    first: {
-      name: Inames
-      time: string
-    }
-    passing: {
-      name: Inames
-      time: string
-      pass_time: string
-    }
-    next: {
-      name: Inames
-      time: string
-    }
-  }
+  lat: number
+  lon: number
+  first_stop: string
+  final_stop: string
 }
 
-const socket = ioClient('http://localhost:3001')
+const path = './raw_data/'
 
-socket.on('connect', () => console.log('connect'))
+function readData(path: string) {
+  return new Promise<string>((resolve, reject) =>
+    readFile(
+      path,
+      (err, data) => (err ? reject(err) : resolve(data.toString()))
+    )
+  )
+}
 
-socket.on('unobus', (buses: Ibus[]) =>
-  buses.forEach(bus => {
-    try {
-      if (bus.stops.passing.name.ja === '') console.log(bus)
-    } catch (err) {
-      console.log(bus)
+function csv(data: string) {
+  return new Promise<string[]>(resolve =>
+    csvParse(
+      data.substr(11),
+      {
+        columns: [
+          'route_num',
+          'okayama_stop_time',
+          'delay',
+          'run',
+          'passing_stop',
+          'license_number',
+          'lat',
+          'lon',
+          'first_stop',
+          'final_stop'
+        ],
+        comment: '//'
+      },
+      (err: Error, busesRaw: IbusRaw[]) =>
+        // resolve(busesRaw.map(bus => bus.first_stop.substr(9))))
+        resolve(busesRaw.map(bus => bus.passing_stop.substr(13)))
+    )
+  )
+}
+
+readdir(path, async (err, files) => {
+  if (err) throw err
+
+  console.time('/libs/unobus.ts')
+
+  const data = await Promise.all(
+    files.map(async filePath =>
+      unobus
+        .get(
+          await readData(path + filePath),
+          moment(filePath.split('.')[0], 'YYYY-MM-DD hh-mm-ss').toISOString()
+        )
+        .catch(err => console.log(filePath))
+    )
+  )
+
+  console.log('Data analysis completed.')
+
+  const fileName = `./test/result/${moment().format('YYYY-MM-DD HH-mm-ss')}.log`
+
+  let errFlag = false
+
+  data.forEach(unobus => {
+    if (!unobus || !unobus.buses) {
+      errFlag = true
+      if (process.argv[2] === 'true') appendFileSync(fileName, data, 'utf-8')
+      return
     }
+
+    if (process.argv[2] === 'true')
+      appendFileSync(
+        fileName,
+        JSON.stringify([...unobus.buses.values()]) + '\n',
+        'utf-8'
+      )
   })
-)
+
+  console.timeEnd('/libs/unobus.ts')
+
+  errFlag
+    ? console.log('An error occurred. Please check the log.')
+    : console.log('unobus.ts test success.')
+  console.log()
+})
