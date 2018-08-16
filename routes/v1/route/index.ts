db2e62578040d0199b7cc236f772edca112a5db6
come25136 <come25136@gmail.com>
2018-08-16 18:45:24 +0900

    :recycle: 一部の変数名を書き換えた

--- routes/v1/route/index.ts
+++ routes/v1/route/index.ts
@@ -2,7 +2,7 @@ import * as moment from 'moment'
 
 import { Router } from 'express'
 
-import { route, getGeo } from '../../../libs/route'
+import { route as _route, getGeo } from '../../../libs/route'
 
 const router = Router({ mergeParams: true })
 
@@ -14,9 +14,9 @@ router.get('/:routeNum', (req, res) =>
 
 // 系統番号と時刻から時刻表を取得
 router.get('/:routeNum/:date', (req, res) =>
-  route(req.params.companyName, req.params.routeNum, moment(req.params.date), true)
-    .then(stops =>
-      res.json(req.query.details === 'true' ? stops : stops.map(trip => trip.map(stop => stop.id)))
+  _route(req.params.companyName, req.params.routeNum, moment(req.params.date), true)
+    .then(route =>
+      res.json(req.query.details === 'true' ? route : route.map(trip => trip.map(stop => stop.id)))
     )
     .catch(err => res.status(404).json({ error: { message: err.message } }))
 )
