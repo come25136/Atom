b04b8b7d179c1e7a1d27e1a28b5df425450dfb43
come25136 <come25136@gmail.com>
2019-12-21 01:05:56 +0900

    :white_check_mark: routesのテストが足りていなかった

--- test/http_server/unobus.co.jp/routes/index.test.ts
+++ test/http_server/unobus.co.jp/routes/index.test.ts
@@ -2,10 +2,28 @@ import { assert } from 'chai'
 import * as req from 'supertest'
 
 import app from '../../../../app'
+import routes from './response'
+import routesDetails from './response/details'
 import route from './route'
 
 describe('routes', () => {
   it('/', async () =>
+    req(app)
+      .get('/v2/unobus.co.jp/routes/')
+      .expect(200)
+      .then(({ body }) =>
+        assert.deepEqual(body, routes)
+      ))
+
+  it('/?details=true', async () =>
+    req(app)
+      .get('/v2/unobus.co.jp/routes?details=true')
+      .expect(200)
+      .then(({ body }) =>
+        assert.deepEqual(body, routesDetails)
+      ))
+
+  it('/1041', async () =>
     req(app)
       .get('/v2/unobus.co.jp/routes/1041/')
       .expect(200)
